0dcb82e5afebaaa6d2beefba3abc339806939e93
EtherLin0606 <2930289360@qq.com>
2026-08-09 15:29:46 +0800

    Site updated: 2026-08-09 15:29:45

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1785655170985";const preCache = ["/images/taichi.png","/images/background.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1786260444977";const preCache = ["/images/taichi.png","/images/background.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
